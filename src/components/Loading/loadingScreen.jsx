54c44fc2eba00b2e0cad30a31d1cb44b26d6fd03
Nosa-Drexx <nosakhare.egharevba@physci.uniben.edu>
2023-07-11 16:34:15 +0100

    loading screen redesigned

--- src/components/Loading/loadingScreen.jsx
+++ src/components/Loading/loadingScreen.jsx
@@ -1,9 +1,21 @@
-import "./loading.scss";
+import HashLoader from "react-spinners/HashLoader";
+import "./loading-screen.scss";
 
 const LoadingScreen = () => {
+  const override = {
+    display: "block",
+    margin: "auto",
+  };
   return (
-    <div className="loading">
-      <span className="fa-solid fa-spinner"></span>
+    <div className="loading-screen">
+      <HashLoader
+        color={`#8A2BE2`}
+        loading={true}
+        size={150}
+        cssOverride={override}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
     </div>
   );
 };
