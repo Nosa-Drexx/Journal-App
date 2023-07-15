f51536bb1d703dd60bca1b226614a1a6eefde145
Nosa-Drexx <nosakhare.egharevba@physci.uniben.edu>
2023-07-16 00:50:28 +0100

    landing page animations enhanced

--- src/pages/HomePage/components/GetStarted/GetStarted.jsx
+++ src/pages/HomePage/components/GetStarted/GetStarted.jsx
@@ -1,24 +1,24 @@
 import "./get-started.scss";
 import RobotImage from "../../images/robot-with-clipboard.png";
 import { Link } from "react-router-dom";
-import { Fade } from "react-reveal";
+import { Fade, Slide } from "react-awesome-reveal";
 
 const GetStarted = () => {
   return (
     <section id="home" className="get-started">
       <div className="call-to-action">
         {/* Fade in main text */}
-        <Fade left duration={1000}>
+        <Slide direction="left" duration={1000} style={{ width: "100%" }}>
           <div className="contain-text">
             <span className="transparent-combinator">&</span>
             <h1>
               Paperless <br /> &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;Easy
             </h1>
           </div>
-        </Fade>
+        </Slide>
 
         {/* Fade in sub text */}
-        <Fade top duration={500} delay={900}>
+        <Fade duration={500} delay={900} style={{ width: "100%" }}>
           <p className="sub-text">
             This is the absolute way to make your notes easier and save the
             earth as well. <br />
@@ -32,15 +32,20 @@ const GetStarted = () => {
 
       {/* Fade illustration */}
 
-      <Fade right delay={500} duration={1000}>
-        <div className="robot-illustration">
+      <div className="robot-illustration">
+        <Slide
+          direction="right"
+          delay={500}
+          duration={1000}
+          style={{ width: "100%", height: "100%" }}
+        >
           <img
             src={RobotImage}
             alt="A robot with a clipboard"
             className="robot-image"
           />
-        </div>
-      </Fade>
+        </Slide>
+      </div>
     </section>
   );
 };
--- src/pages/HomePage/components/WorldAnimation/WorldAnimationSection.jsx
+++ src/pages/HomePage/components/WorldAnimation/WorldAnimationSection.jsx
@@ -1,27 +1,52 @@
-// import * as LottiePlayer from "@lottiefiles/lottie-player";
-import { useEffect } from "react";
+import * as LottiePlayer from "@lottiefiles/lottie-player";
+import { create } from "@lottiefiles/lottie-interactivity";
+import { useEffect, useRef } from "react";
 import "./world-animation.scss";
-import { Fade } from "react-reveal";
+import { Slide } from "react-awesome-reveal";
 
 const WorldLottieAnimation = () => {
+  const player = useRef(null);
+  // useEffect(() => {
+  //   import("lottie-interactive/dist/lottie-interactive.js");
+  // });
+
   useEffect(() => {
-    import("lottie-interactive/dist/lottie-interactive.js");
-  });
+    create({
+      player: "#worldMap",
+      mode: "cursor",
+      actions: [
+        {
+          position: { x: [0, 1], y: [-1, 2] },
+          type: "seek",
+          transition: "click",
+          frames: [0, 840],
+        },
+      ],
+    });
+  }, [player]);
 
   return (
-    <Fade bottom>
-      <section className="world-animation-section">
+    <section className="world-animation-section">
+      <Slide direction="up" style={{ width: "100%", height: "100%" }}>
         <div className="lottie-container">
-          <lottie-interactive
+          {/* <lottie-interactive
             path="https://lottie.host/9e28bb4e-f2e4-46ad-a6a3-d279cb604f57/vnZp0Vs5yn.json"
             interaction="freeze-click"
             class="lottie-interactive-container"
             autoplay
             loop
-          ></lottie-interactive>
+          ></lottie-interactive> */}
+          <lottie-player
+            autoplay
+            loop
+            mode="normal"
+            src="https://lottie.host/9e28bb4e-f2e4-46ad-a6a3-d279cb604f57/vnZp0Vs5yn.json"
+            id="worldMap"
+            ref={player}
+          ></lottie-player>
         </div>
-      </section>
-    </Fade>
+      </Slide>
+    </section>
   );
 };
 
